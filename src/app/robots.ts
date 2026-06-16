b8587a3736139e7e159694f614939dafe5c69fc2
Amar Smajlovic <amar.smajlovic@ministryofprogramming.com>
2026-06-16 19:28:10 +0200

    feat: Domain switchin changes

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -7,6 +7,6 @@ export default function robots(): MetadataRoute.Robots {
       allow: '/',
       disallow: ['/api/', '/_next/'],
     },
-    sitemap: 'https://app.autocapgroup.se/sitemap.xml',
+    sitemap: 'https://autocapgroup.se/sitemap.xml',
   };
 }
